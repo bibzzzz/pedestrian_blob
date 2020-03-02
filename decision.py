d2dc406471e51314c04bd5d601c10ae627abfb85
bibzzzz <habib.adam87@gmail.com>
2020-03-02 15:01:29 -0500

    simulator and decision functions working

--- decision.py
+++ decision.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import itertools
+import math
 
 def execute_decision(x, y, move_x, move_y):
     x_new = x + move_x
@@ -7,41 +9,46 @@ def execute_decision(x, y, move_x, move_y):
 
     return x_new, y_new
 
-def calc_decision(x, y, target_x, target_y, x_open, y_open, coord_range, move_type='manhattan', traffic='off', use_intel=0):
+def calc_decision(x, y, target_x, target_y, x_open, coord_range, move_type='manhattan',
+                  traffic='off', use_intel=0, intel_version=0, expl_rate=0):
 
     if x_open==1:
         x_moves = [-1, 0, 1]
-        y_moves [0]
+        y_moves = [0]
     else:
-        x_moves [0]
+        x_moves = [0]
         y_moves = [-1, 0, 1]
 
-    x_proj = x_moves + x
-    y_proj = y_moves + y
+    x_moves = [i for i in x_moves if abs(i + x) <= coord_range]
+    y_moves = [i for i in y_moves if abs(i + y) <= coord_range]
+    #print(x_moves)
+    #print(y_moves)
 
-    print(x_moves)
-    x_moves = x_moves[abs(x_proj) > coord_range]
-    y_moves = y_moves[abs(y_proj) > coord_range]
-    print(x_moves)
-
-    xy_moves = list(itertools.combinations(x_moves, y_moves))
-    print(xy_moves)
+    xy_moves = list(itertools.product(x_moves, y_moves))
+    #print(xy_moves)
 
     if use_intel==0:
         #pick random move from move list
-        decision_index = np.random.randint(0, len(xy_moves), 1)[0]
-        (x_move, y_move) = xy_moves[decision_index]
+        proj_vec = np.zeros(len(xy_moves))
+        #decision_index = np.random.randint(0, len(xy_moves), 1)[0]
     else:
-        predict_outcome = predict(sdjlaksldja)
+        proj_vec = predict(sdjlaksldja)
 
-    print(xy_moves)
+    rand_adj_vec = np.random.uniform(0, 1, len(xy_moves))
+    #print(rand_adj_vec)
+    result_vec = proj_vec + (expl_rate)*rand_adj_vec
+    #print(result_vec)
+    decision_index = np.argmax(result_vec)
+    #print(decision_index)
+    (x_move, y_move) = xy_moves[decision_index]
+    #print((x_move, y_move))
 
     return (x_move, y_move)
 
 
-if __name__ == '__main__':
-
-    xy_move = calc_decision(x=0, y=0, target_x=2, target_y=2, coord_range=1, move_type='manhattan', traffic='off', use_intel=0)
-
-    print(xy_move[0])
-    print(xy_move[1])
+#if __name__ == '__main__':
+#
+#    xy_move = calc_decision(x=0, y=0, target_x=2, target_y=2, x_open=0, coord_range=1, move_type='manhattan', traffic='off', use_intel=0, intel_version=0)
+#
+#    print(xy_move[0])
+#    print(xy_move[1])
--- simulator.py
+++ simulator.py
@@ -5,9 +5,9 @@ import math
 import uuid
 import six.moves.cPickle as pickle
 import os
-from datetime import datetime, date, timedelta
-from simulator_input import get_sim_data
-from mlp import predict_mlp_given_x
+import csv
+from multiprocessing.dummy import Pool as ThreadPool
+from decision import calc_decision, execute_decision
 
 
 class BlobSimulation():
@@ -16,20 +16,22 @@ class BlobSimulation():
         class object that simulates a complete pedestrian blob attempt at reaching target coords and records the results
     '''
 
-    def __init__(self, x_start, y_start, x_target, y_target, coord_range, move_limit,
-                 data_dir, use_intel=0, move_types='manhattan', traffic='none'):
+    def __init__(self, coord_range, move_limit, data_dir, use_intel=0, expl_rate=0, move_type='manhattan', traffic='none'):
 
         self.simID = str(uuid.uuid4())
         self.data_dir = data_dir
 
         self.use_intel = use_intel
+        self.expl_rate = expl_rate
 
-        self.x = x_start
-        self.y = y_start
-        self.x_target = x_target
-        self.y_target = y_target
+        self.x = np.random.randint(-coord_range, coord_range, 1)[0]
+        self.y = np.random.randint(-coord_range, coord_range, 1)[0]
+        self.target_x = np.random.randint(-coord_range, coord_range, 1)[0]
+        self.target_y = np.random.randint(-coord_range, coord_range, 1)[0]
 
-        self.move_types = move_types
+        self.coord_range = coord_range
+
+        self.move_type = move_type
         self.traffic = traffic
         self.move_limit = move_limit
 
@@ -46,28 +48,83 @@ class BlobSimulation():
 
     def simulate(self):
 
-        pre_decisionList = []
-        post_decisionList = []
-        while (self.n_moves < self.move_limit) or (self.x == self.target_x and self.y == self.target_y):
+        self.pre_decisionList = []
+        self.post_decisionList = []
 
-            pre_decisionList = pre_decisionList.append([self.simID, self.n_moves, self.x, self.y, self.target_x, self.target_y, self.coord_range])
+        print('initializing simID %s: starting at (%s, %s) with target (%s, %s) and %s moves' %(self.simID, self.x, self.y, self.target_x, self.target_y, self.move_limit))
 
-            print('executing move %s...' %(self.n_moves))
+        while (self.n_moves < self.move_limit) and (self.sim_result == 0):
+
+            self.pre_decisionList.append([self.simID, 'pre', self.n_moves, self.x, self.y, self.target_x, self.target_y, self.coord_range])
+
+            #print('calculating move %s...' %(self.n_moves))
             x_open = np.random.randint(0,2,1)[0]
 
-            (x_move, y_move) = calc_decision(x=self.x, y=self.y, target_x=self.target_x,
-                                             target_y=self.target_y, x_open=x_open,
-                          coord_range=self.coord_range, move_type=self.move_type,
-                          traffic=self.traffic, use_intel=self.use_intel,
-                          intel_version=self.intel_version)
+            (x_move, y_move) = calc_decision(x=self.x, y=self.y, target_x=self.target_x, target_y=self.target_y, x_open=x_open, coord_range=self.coord_range, move_type=self.move_type, traffic=self.traffic, use_intel=self.use_intel, intel_version=self.intel_version, expl_rate=self.expl_rate)
 
-            print('executing move %s...' %(self.n_moves))
+            #print('executing move %s... x:%s, y:%s' %(self.n_moves, x_move, y_move))
             self.x, self.y = execute_decision(x=self.x, y=self.y, move_x=x_move, move_y=y_move)
 
             self.n_moves += 1
-            post_decisionList = post_decisionList.append([self.simID, self.n_moves, self.x, self.y, self.target_x, self.target_y, self.coord_range])
+            self.post_decisionList.append([self.simID, 'post',  self.n_moves, self.x, self.y, self.target_x, self.target_y, self.coord_range])
+
+            #print(self.x, self.y)
 
             if (self.x == self.target_x and self.y == self.target_y):
+                print('%s TARGET REACHED in %s MOVES!' %(self.simID, self.n_moves))
                 self.sim_result += 1
+
+            if self.n_moves == self.move_limit:
+                print('%s MOVE LIMIT EXCEEDED!' %(self.simID))
+
+
+if __name__ == '__main__':
+
+    # processing settings
+    n_simulations = 10
+    n_workers = 4
+
+    # sim settings
+    expl_rate = 0.2
+
+    # define simulation function (for parallelerization)
+    def simulate(simulation):
+        simulation.simulate()
+
+    data_dir = os.path.dirname(os.path.realpath(__file__)) + '/sim_data/'
+    data_filepath = os.path.dirname(os.path.realpath(__file__)) + '/sim_data/%expl_rate, '
+
+    # create a fleet of simulations, and store them in a list
+    sims = [BlobSimulation(coord_range=4, move_limit=100, data_dir=data_dir, use_intel=0, expl_rate=expl_rate, move_type='manhattan', traffic='none') for x in range(0,n_simulations)]
+
+    # make the Pool of workers
+    pool = ThreadPool(n_workers)
+
+    # threaded simulation
+    results = pool.map(simulate, sims)
+
+    #close the pool and wait for the work to finish
+    pool.close()
+    pool.join()
+
+    # store results
+    # specify filepaths
+
+    for sim in sims:
+
+        # if output file doesnt exist create it
+        if not os.path.exists(data_filepath):
+            with open(data_filepath,'w') as f:
+                sim_output = csv.writer(f)
+                rowEntry = ['simID', 'decision_stage', 'n_moves', 'blob_x', 'blob_y', 'target_x', 'target_y', 'coord_range', 'sim_move_limit', 'sim_move_total', 'sim_result']
+                sim_output.writerow(rowEntry)
+        # open files for appending
+        with open(data_filepath,'a') as f:
+            sim_output = csv.writer(f)
+            for entry in sim.pre_decisionList:
+                rowEntry = entry
+                rowEntry.extend([sim.move_limit, sim.n_moves, sim.sim_result])
+                sim_output.writerow(rowEntry)
+            f.close()
 
 
